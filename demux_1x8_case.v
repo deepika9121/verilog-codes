afd06c17656032e83ddccfe724020e564d317a12
deepika9121 <bothsadeepika2004@gmail.com>
2024-09-30 16:49:45 +0530

    demux_1x8_case.v

--- demux_1x8_case.v
+++ demux_1x8_case.v
@@ -11,45 +11,37 @@ begin
 case(sel)
 3'b000:
     begin 
-    out[0]=in;
-    out[1]=0;out[2]=0;out[3]=0; out[4]=0;out[5]=0;out[6]=0;out[7]=0;
+        out=8'b0000_0001;
     end
 3'b001:
     begin
-    out[1]=in;
-      out[0]=0;out[2]=0;out[3]=0; out[4]=0;out[5]=0;out[6]=0;out[7]=0;
-    end
+    out=8'b0000_0010;
+      end
 3'b010:
     begin 
-    out[2]=in;
-      out[1]=0;out[0]=0;out[3]=0; out[4]=0;out[5]=0;out[6]=0;out[7]=0;
+    out=8'b0000_0100;
     end
 3'b011:
     begin
-    out[3]=in;
-      out[1]=0;out[2]=0;out[0]=0; out[4]=0;out[5]=0;out[6]=0;out[7]=0;
+    out=8'b0000_1000;
     end  
 3'b100:
     begin 
-    out[4]=in;
-    out[1]=0;out[2]=0;out[3]=0; out[0]=0;out[5]=0;out[6]=0;out[7]=0;
+    out=8'b0001_0000;
     end
 3'b101:
     begin
-    out[5]=in;
-      out[0]=0;out[2]=0;out[3]=0; out[4]=0;out[1]=0;out[6]=0;out[7]=0;
+    out=8'b0010_0000;
     end
 3'b110:
     begin 
-    out[6]=in;
-      out[1]=0;out[0]=0;out[3]=0; out[4]=0;out[5]=0;out[2]=0;out[7]=0;
+    out=8'b0100_0000;
     end
 3'b111:
     begin
-    out[7]=in;
-      out[1]=0;out[2]=0;out[0]=0; out[4]=0;out[5]=0;out[6]=0;out[3]=0;
+    out=8'b1000_0000;
     end  
-default :out[7:0]=8'bxxxxxxxx;
+    default :out=8'bxxxx_xxxx;
 endcase
 end
 endmodule
