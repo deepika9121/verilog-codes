aadbb0824b345261a1e7399c344996babdf47a18
deepika9121 <bothsadeepika2004@gmail.com>
2024-09-30 11:57:00 +0530

    demux_1x4_ifelse.v

--- demux_1x4_ifelse.v
+++ demux_1x4_ifelse.v
@@ -8,26 +8,20 @@ always@*
 begin
 if(sel==2'b00)
     begin 
-    out[0]=in;
-    out[1]=0;out[2]=0;out[3]=0;
+    out=4'b0001
     end
 else if(sel==2'b01)
     begin
-    out[1]=in;
-    out[0]=0;out[2]=0;out[3]=0;
+    out=4'b0010;
     end 
 else if(sel==2'b10)
     begin 
-    out[2]=in;
-    out[1]=0;out[0]=0;out[3]=0;
+    out=4'b0100;
     end
 else if(sel==2'b11)
     begin
-    out[3]=in;
-    out[0]=0;out[2]=0;out[1]=0;
+out=4'b1000;
     end 
 else
-out[3:0]=4'bxxxx;
-
-end
+    out=4'bxxxx;
 endmodule
